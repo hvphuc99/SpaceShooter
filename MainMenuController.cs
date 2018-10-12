a193870ec6ee9761543a0cde8463d686483f1dae
Hoang Phuc <hvphuc081299@gmail.com>
2018-10-12 20:37:57 +0700

    Tao menu co nut GamePlay va Quit

---
+++ MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void PlayGameButton() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("GamePlay");
+
+	}
+	public void QuitGameButton() {
+		Application.Quit ();
+	}
+}
