4a20c029edbe7d4d1734867f970b1e49abfbb1c6
Hoang Phuc <hvphuc081299@gmail.com>
2018-11-04 22:21:50 +0700

    Contain scripts

---
+++ Scripts/GamePlayController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayController : MonoBehaviour {
+	public static GamePlayController instance;
+	void Awake() {
+		MakeInstance ();
+	}
+	void MakeInstance() {
+		if (instance == null)
+			instance = this;
+	}
+		
+
+	[SerializeField]private GameObject pausepanel, gameOverPanel;
+	public void PauseGameButton() {
+		pausepanel.SetActive (true);
+		Time.timeScale = 0f;
+	}
+	public void ResumeGameButton() {
+		pausepanel.SetActive (false);
+		Time.timeScale = 1f;
+	}
+	public void RestartGameButton() {
+		gameOverPanel.SetActive (false);
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("GamePlay");
+	}
+	public void OptionsButton() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("MainMenu");
+	}
+	public void PlaneDiedShowPanel() {
+		gameOverPanel.SetActive (true);
+	}
+}
